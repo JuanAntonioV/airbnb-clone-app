8f0c7daaff57453c202ea3f86547e2f784176cec
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-09-20 16:56:52 +0700

    project setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div>
-            <h1>Helo Airbnb</h1>
+            <h1 className='text-rose-500 text-2xl'>Helo Airbnb</h1>
         </div>
     );
 }
